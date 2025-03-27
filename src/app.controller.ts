77eead44ce1091de997c3e3e2edd782047eabe56
Luiz Davi <luizdaviinformatica@gmail.com>
2025-03-27 12:58:43 -0300

    fix: healthcheck is public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { name } from '../package.json';
+import { Public } from '@src/auth/decorators/public.decorator';
 
 @ApiTags('Health-check')
 @Controller()
 export class AppController {
   @Get('/health')
+  @Public()
   healthCheck(): string {
     return `Service ${name} is up and running`;
   }
